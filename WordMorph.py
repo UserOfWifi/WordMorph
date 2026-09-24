413f4eaa30927a5f04c2c1ba44a362f5e3f569c8
Jesus_Wifi <villagranajesus932@gmail.com>
2026-09-23 21:41:58 -0500

    Adds new line to the end of the file

--- WordMorph.py
+++ WordMorph.py
@@ -1,3 +1,5 @@
 #This is currently just an experiment to see if git hub works.
 #Once everything is set these comments can be removed
 print("Hello World!")
+x = 21
+print("What is " + x + "when divided by" + (x/3) + "?" "\nIt's " + (x/7) + "!!")
